059e3765bbb3b0e34ddd60ea2e1e886a51d34dc5
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-20 21:55:03 +0530

    [Priyansh]Added:Ability to create contact in Address Book

---
+++ AddressBookMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Address_Book_Application
+{
+    class AddressBookMain
+    {
+        ArrayList records = new ArrayList();
+        public void addPerson()
+        {
+            string firstName = "", lastName = "", address = "", city = "", state = "", phoneNo = "",zip="";
+
+            Console.WriteLine("Enter first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine(firstName);
+
+            Console.WriteLine("Enter last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine(lastName);
+
+            Console.WriteLine("Enter address:");
+            address = Console.ReadLine();
+            Console.WriteLine(address);
+
+            Console.WriteLine("Enter city:");
+            city = Console.ReadLine();
+            Console.WriteLine(city);
+
+            Console.WriteLine("Enter state:");
+            state = Console.ReadLine();
+            Console.WriteLine(state);
+
+            Console.WriteLine("Enter zip:");
+            zip = Console.ReadLine();
+            Console.WriteLine(zip);
+
+            Console.WriteLine("Enter phone no:");
+            phoneNo = Console.ReadLine();
+            Console.WriteLine(phoneNo);
+
+            Person p = new Person(firstName, lastName, address, city, state, zip, phoneNo);
+            records.Add(p);
+            Console.WriteLine("Added Successfully");
+
+        }
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book_Application
+{
+    class Person
+    {
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        string zip;
+        string phoneNo;
+        public Person(string firstName, string lastName, string address, string city, string state, string zip, string phoneNo)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.address = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNo = phoneNo;
+        }
+        public string getFirstName() 
+        {
+            return this.firstName;
+        }
+        public string getLastName()
+        {
+            return this.lastName;
+        }
+        public string getAddress() 
+        {
+            return this.address;
+        }
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+        public string getState()
+        {
+            return this.state;
+        }
+        public void setState(string state)
+        {
+            this.state = state;
+        }
+        public string getCity()
+        {
+            return this.city;
+        }
+        public void setCity(string city)
+        {
+            this.city = city;
+        }
+        public string getzip()
+        {
+            return this.zip;
+        }
+        public void setZip(string zip)
+        {
+            this.zip = zip;
+        }
+        public string getPhoneNo()
+        {
+            return this.phoneNo;
+        }
+        public void setPhoneNo(string phoneNo)
+        {
+            this.phoneNo = phoneNo;
+        }
+    }
+}
